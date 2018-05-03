444015fc4df91428a496e27edcf13a60d55fda81
Martín Díaz <magadiflo@gmail.com>
2018-05-03 12:35:39 -0500

    Se implementó el registro de editorialControl.java, EditorialDao.java y la vista registroEditorial.jsp funcionando correctamente.

---
+++ src/java/dao/EditorialDao.java
@@ -0,0 +1,61 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Editorial;
+
+public class EditorialDao {
+    
+     public static boolean registrar(Editorial editorial){
+        try {
+            String sql = "INSERT INTO Editorial(nit, nombre, telefono, direccion,"
+                    + "email, sitioWeb) "
+                    + "VALUES(?,?,?,?,?,?)";
+            Connection con = Conexion.connectar();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, editorial.getNit());
+            ps.setString(2, editorial.getNombre());
+            ps.setString(3, editorial.getTelefono());
+            ps.setString(4, editorial.getDireccion());
+            ps.setString(5, editorial.getEmail());
+            ps.setString(6, editorial.getSitioWeb());
+            
+            if(ps.executeUpdate() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException ex) {
+            return false;
+        }  
+    }
+    
+    public static ArrayList<Editorial> listar(){
+        try {
+            String sql = "SELECT * FROM Editorial";
+            Connection con = Conexion.connectar();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            ArrayList<Editorial> lista = new ArrayList<>();
+            Editorial editorial;
+            while(rs.next()){
+                editorial = new Editorial();
+                editorial.setNit(rs.getString(1));
+                editorial.setNombre(rs.getString(2)); 
+                editorial.setTelefono(rs.getString(3));
+                editorial.setDireccion(rs.getString(4));
+                editorial.setEmail(rs.getString(5));
+                editorial.setSitioWeb(rs.getString(6));
+                
+                lista.add(editorial);
+            }
+            return lista;
+        } catch (SQLException ex) {
+            return null;            
+        }  
+    }
+    
+}
